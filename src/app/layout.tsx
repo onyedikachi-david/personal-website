ee8e8ad9eb1eada26fe62c6276d0351f59bdd46c
David Anyatonwu <davidanyatonwu@gmail.com>
2024-11-15 00:12:31 +0100

    del: unused component
    Signed-off-by: David Anyatonwu <davidanyatonwu@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import '@fontsource/fira-code'
-import { stylesheets } from '@/lib/mdx-config'
 import Navigation from '@/components/Navigation'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,13 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        {stylesheets.map((href) => (
-          <link key={href} rel="stylesheet" href={href} />
-        ))}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </head>
       <body className={inter.className}>
         <Navigation />
         <main className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
